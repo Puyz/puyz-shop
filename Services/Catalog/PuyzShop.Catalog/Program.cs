87960e48a249e666e19d524cb746baf3152c1977
Ömer temel <puyzsoft@gmail.com>
2024-05-19 15:40:54 +0300

    service scope added

--- Services/Catalog/PuyzShop.Catalog/Program.cs
+++ Services/Catalog/PuyzShop.Catalog/Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.Extensions.Options;
+using PuyzShop.Catalog.Services.CategoryServices;
+using PuyzShop.Catalog.Services.ProductDetailServices;
+using PuyzShop.Catalog.Services.ProductImageServices;
+using PuyzShop.Catalog.Services.ProductServices;
+using PuyzShop.Catalog.Settings;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductDetailService, ProductDetailService>();
+builder.Services.AddScoped<IProductImageService, ProductImageService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
