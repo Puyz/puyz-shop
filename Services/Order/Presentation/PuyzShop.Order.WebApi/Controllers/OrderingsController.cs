f802f6096c5193497b502ad7716b94b1102cf4be
Ömer temel <puyzsoft@gmail.com>
2024-05-26 17:40:03 +0300

    order: added controller

---
+++ Services/Order/Presentation/PuyzShop.Order.WebApi/Controllers/OrderingsController.cs
@@ -0,0 +1,56 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PuyzShop.Order.Application.Features.Mediator.Commands.OrderingCommands;
+using PuyzShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
+using PuyzShop.Order.Application.Features.Mediator.Results.OrderingResults;
+
+namespace PuyzShop.Order.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderingsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderingsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var values = await _mediator.Send(new GetOrderingQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var values = await _mediator.Send(new GetOrderingByIdQuery(id));
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateOrderingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Sipariş başarıyla eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Remove(int id)
+        {
+            await _mediator.Send(new RemoveOrderingCommand(id));
+            return Ok("Sipariş başarıyla silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateOrderingCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Sipariş başarıyla güncellendi");
+        }
+    }
+}
--- Services/Order/Presentation/PuyzShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/PuyzShop.Order.WebApi/Program.cs
@@ -1,10 +1,16 @@
 using PuyzShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
 using PuyzShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using PuyzShop.Order.Application.Interfaces;
+using PuyzShop.Order.Application.Services;
+using PuyzShop.Order.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddApplicationService(builder.Configuration);
+
+#region
 builder.Services.AddScoped<GetAddressQueryHandler>();
 builder.Services.AddScoped<GetAddressByIdQueryHandler>();
 builder.Services.AddScoped<CreateAddressCommandHandler>();
@@ -16,6 +22,9 @@ builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
 builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
 builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
 builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
+#endregion
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
